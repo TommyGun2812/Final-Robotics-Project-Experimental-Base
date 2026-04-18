b0838f65f19d10d36803e81d3fa23bc60f5c81b9
TommyGun2812 <perezveratomas08@gmail.com>
2026-04-17 21:02:28 -0600

    first project base structure

---
+++ puzzlebot_ws/build/puzzlebot_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tom/puzzlebot_ws/src/puzzlebot_ros2/puzzlebot_gazebo
+# Build directory: /home/tom/puzzlebot_ws/build/puzzlebot_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ puzzlebot_ws/build/puzzlebot_navigation2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tom/puzzlebot_ws/src/puzzlebot_ros2/puzzlebot_navigation2
+# Build directory: /home/tom/puzzlebot_ws/build/puzzlebot_navigation2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
